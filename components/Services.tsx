9ed6cdbda7c1e0af7b5b9414d11a2e9a933ca05d
ranselorm <ranselorm@outlook.com>
2025-02-04 20:57:48 +0000

    Refactor service card

--- components/Services.tsx
+++ components/Services.tsx
@@ -5,9 +5,9 @@ import { ServiceCard } from "./ServiceCard";
 
 const Services = () => {
   return (
-    <section className="bg-dark h-full font-eaves">
-      <div className="">
-        <div className="text-center mb-12 text-white flex-col flex gap-y-2">
+    <section className="bg-dark h-full font-eaves section-padding">
+      <div className="container mx-auto px-4">
+        <div className="text-center mb-8 text-white flex-col flex gap-y-2">
           <p className="text-xl font-medium text-primary">My Expertise</p>
           <h3 className="section-title text">Innovative solutions</h3>
           <p className="max-w-2xl mx-auto mb-8 font-xl">
@@ -15,39 +15,27 @@ const Services = () => {
             quisquam unde nemo illum reiciendis
           </p>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-          <div className="grid grid-cols-4 gap-12">
-            <ServiceCard
-              icon="mdi:finance"
-              title="Finance Planning"
-              description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
-            />
-            <ServiceCard
-              icon="mdi:chart-bar"
-              title="Investment Planning"
-              description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
-            />
-            <ServiceCard
-              icon="mdi:percent"
-              title="Mutual Funds"
-              description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
-            />
-            <ServiceCard
-              icon="mdi:shield-home"
-              title="Personal Insurance"
-              description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
-            />
-            <ServiceCard
-              icon="mdi:cash-multiple"
-              title="Market Research"
-              description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
-            />
-            <ServiceCard
-              icon="mdi:chart-pie"
-              title="Business Analysis"
-              description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
-            />
-          </div>
+        <div className="grid grid-cols-4 gap-4">
+          <ServiceCard
+            icon="mdi:finance"
+            title="Finance Planning"
+            description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
+          />
+          <ServiceCard
+            icon="mdi:chart-bar"
+            title="Investment Planning"
+            description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
+          />
+          <ServiceCard
+            icon="mdi:percent"
+            title="Mutual Funds"
+            description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
+          />
+          <ServiceCard
+            icon="mdi:shield-home"
+            title="Personal Insurance"
+            description="Nanotechnology immersion along the information highway will close the loop on focusing solely."
+          />
         </div>
       </div>
     </section>

72d2b56607cb717408b203f1ea9dbf14c4bea607
ranselorm <ranselorm@outlook.com>
2025-04-02 22:25:36 +0000

    Add text section

--- components/About.tsx
+++ components/About.tsx
@@ -29,23 +29,29 @@ const About = () => {
       viewport={{ once: true, amount: 0.3 }}
     >
       <motion.div
-        className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-12"
+        className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-12"
         variants={containerVariants}
       >
-        <motion.div className="" variants={containerVariants}>
+        <motion.div className="md:max-w-xl" variants={containerVariants}>
           <h3 className="text-2xl font-semibold mb-4">About me</h3>
           <p className="leading-normal text-gray-900 mb-4 text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
             eveniet! Dicta exercitationem sequi vero ab, illum laborum enim modi
             provident a mollitia, velit sit! Voluptatibus accusantium obcaecati
             quisquam enim nam! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Fugiat odio deleniti aut. Iure quod a explicabo,
-            voluptatibus quisquam illum cum non molestias omnis dignissimos illo
-            amet aliquam, soluta, veniam harum.
+          </p>
+          <p className="leading-normal text-gray-900 text-lg mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            eveniet! Dicta exercitationem sequi vero ab, illum laborum enim modi
+            provident a mollitia, velit sit! Voluptatibus accusantium obcaecati
+            quisquam enim nam! Lorem ipsum dolor sit amet consectetur
+          </p>
+          <p className="leading-normal text-gray-900 text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
           </p>
         </motion.div>
-        <motion.div className="flex justify-center" variants={itemVariants}>
-          <div className="relative w-[350px] h-[400px]">
+        <motion.div className="flex" variants={itemVariants}>
+          <div className="relative w-[400px] h-[400px]">
             <div className="absolute inset-0 bg-primary rounded-xl transform translate-x-10 translate-y-10 z-0">
               <img
                 src="/images/bg.svg"

eff5df483206214bb472c4708defcb9dfb3a95e0
ranselorm <ranselorm@outlook.com>
2025-06-21 17:53:23 +0000

    Add MainHero component

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import Navbar from "@/components/Navbar";
 import Portfolio from "@/components/Portfolio";
 import Services from "@/components/Services";
 import Skills from "@/components/Skills";
+import MainHero from "../components/MainHero";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       {/* <Hero /> */}
+      <MainHero />
       <HeroSection />
       <Services />
       <About />

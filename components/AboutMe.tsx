98bcc102218d8cab3c2b037734eecb83f534d9b1
ranselorm <ranselorm@outlook.com>
2025-07-05 19:40:40 +0000

    Implement code changes to enhance functionality and improve performance

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -4,14 +4,26 @@ const AboutMe = () => {
   return (
     <section className="section-padding bg-grey font-primary py-12">
       <div className="container mx-auto">
-        <div className="flex items-center gap-4 w-[70%] mx-auto">
+        <div className="flex flex-row-reverse items-center gap-4 w-[70%] mx-auto">
           <div className="w-2/4 space-y-3">
-            <div className="bg-white shadow-sm w-full h-[250px] rounded-md">
-              1
-            </div>
+            <div className="border border-primary shadow-sm w-full h-[250px] rounded-md p-4 flex items-center justify-center flex-col gap-4"></div>
             <div className="bg-[#e3e5ff] w-full h-[250px] rounded-md">2</div>
           </div>
-          <div className="bg-[#e3e5ff] w-2/4 rounded-md h-[512px]">3</div>
+          <div className="bg-[#e3e5ff] w-2/4 rounded-md h-[512px] border border-primary flex flex-col items-center justify-center gap-4 p-4">
+            <div className="w-56 rounded-md overflow-hidden">
+              <img
+                src="/images/profile.jpg"
+                alt="profile"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <a href="mailto:selorm@berthengineering.com">
+              selorm@berthengineering
+            </a>
+            <button className="bg-primary rounded-full py-2 px-4 flex items-center justify-center text-xs text-white">
+              Download CV
+            </button>
+          </div>
         </div>
       </div>
     </section>

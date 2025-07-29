69cbb4c34a20e95bde17da8f68edb52ebede7c0b
ranselorm <ranselorm@outlook.com>
2025-07-29 22:54:06 +0000

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 import About from "@/components/About";
-import Blog from "@/components/Blog";
-import Connect from "@/components/Connect";
-import Contact from "@/components/Contact";
+
 import Hero from "@/components/Hero";
-import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-import Portfolio from "@/components/Portfolio";
-import Services from "@/components/Services";
-import Skills from "@/components/Skills";
-import MainHero from "../components/MainHero";
-import AboutMe from "@/components/AboutMe";
+
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
-import SkillsTree from "@/components/SkillsTree";
 import FeaturedTestimonial from "@/components/FeaturedTestimonial";
 
 export default function Home() {
